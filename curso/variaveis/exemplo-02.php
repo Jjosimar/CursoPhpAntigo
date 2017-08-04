ba880e5c0d74e6e08c1626a096a69f5dee070947
Jjosimar <josimar.infotecnico@gmail.com>
2017-08-04 18:02:16 -0300

    Iniciando o curso

---
+++ curso/variaveis/exemplo-02.php
@@ -0,0 +1,15 @@
+<?php
+
+$anoNascimento = 1990;
+$nomeCompleto = "";
+$nome1 = "joao";
+
+echo $nome1;
+//apagar variavel
+echo "<br/>";
+unset($nome1);
+
+echo $nome1;
+
+//continuação na aula 14
+?>
